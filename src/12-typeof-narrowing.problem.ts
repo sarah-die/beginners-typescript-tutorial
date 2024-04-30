7e17ffe23b7738c76b32d7498d271ba0a5b143e2
Sarah Diethert <sarah.diethert@sd-c.de>
2024-04-30 15:36:17 +0200

    solution: 12

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,16 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+// write this function
+// note: you can´t access a property we don´t know is there
+const coerceAmount = (amount: number | { amount: number }) => {
+  // solution 1
+  // if (typeof amount === "number") return amount;
+  // return amount.amount;
+
+  // solution 2
+  if (typeof amount === "object") return amount.amount;
+  return amount;
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
