383fba599cae8801e38bf85d4b684e24263b2d0d
Sarah Diethert <sarah.diethert@sd-c.de>
2024-04-30 14:16:20 +0200

    solution: 02

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,26 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+// note: a type can represent anything (number, object, boolean)
+// better to extract the type to make error messages etc. more readable
+type AddTwoNumbersProps = {
+  first: number;
+  second: number;
+};
+
+// note: interfaces can only be used to present objects (and a couple of other things)
+interface AddTwoNumbersInterface {
+  first: number;
+  second: number;
+}
+
+export const addTwoNumbers = (
+  // solution 1: use props-type
+  params: AddTwoNumbersProps
+  // solution 2: parse in object type
+  // { first: number; second: number }
+  // solution 3: use props-interface
+  // AddTwoNumbersInterface
+) => {
   return params.first + params.second;
 };
 
@@ -9,13 +29,13 @@ it("Should add the two numbers together", () => {
     addTwoNumbers({
       first: 2,
       second: 4,
-    }),
+    })
   ).toEqual(6);
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    }),
+    })
   ).toEqual(30);
 });
