09e08862ff474a4adb0e61372a3cd1115d033a30
Sarah Diethert <sarah.diethert@sd-c.de>
2024-04-30 14:46:28 +0200

    solution: 09

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -9,8 +9,32 @@ interface LukeSkywalker {
   gender: string;
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
+// solution 1: utility type
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
+
+// solution 2: type casting
+// not safe (prefer 1 or 3)
+export const fetchLukeSkywalker2 = async () => {
+  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data as LukeSkywalker;
+};
+
+// solution 3: type data
+// the proper return type is inferred because it´s an async function
+export const fetchLukeSkywalker3 = async (): Promise<LukeSkywalker> => {
+  // this syntax makes sure it´s assignable to Luke Skywalker
+  const data: LukeSkywalker = await fetch(
+    "https://swapi.dev/api/people/1"
+  ).then((res) => {
     return res.json();
   });
 
